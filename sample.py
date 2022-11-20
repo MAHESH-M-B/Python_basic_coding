23f5b5b65b46eb16b6090f8da7733dc8093575e1
maheh <maheshmb7712@gmail.com>
2022-11-20 08:01:06 +0530

    programmms

--- sample.py
+++ sample.py
@@ -1,13 +1,27 @@
-values=['mahesh','mukesh','maneesh']
+# values=['mahesh','mukesh','maneesh']
 
-print(values[1])
-print(values[0:2])
+# print(values[1])
+# print(values[0:2])
 
-values=values+['1','2','3']
-print(values)
+# values=values+['1','2','3']
+# print(values)
 
 
-b=100
-values.append(b)
+# b=100
+# values.append(b)
 
-print(values)
+# print(values)
+
+
+
+
+x='global x'
+
+
+def test():
+    global x
+    x='local x'
+    print(x)
+
+test()
+print(x)
